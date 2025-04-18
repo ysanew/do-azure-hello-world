5477de5a112f32bcbd9026d9b94a4777514f6998
Yurii <osnew0502@gmail.com>
2025-04-18 14:39:12 -0400

    fix: use globally unique app_name for App Service

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "resource_group_name" { default = "rg-hello-world" }
 variable "location" { default = "Canada Central" }
-variable "app_name" { default = "hello-world-app" }
+variable "app_name" { default = "hello-world-app-ysanew-2304" }
